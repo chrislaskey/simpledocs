1e04b36c568cbe22ce7007c11c1ebd472be8cc51
Chris Laskey <contact@chrislaskey.com>
2013-06-07 16:23:53 -0400

    Updated file name parsing and display

--- application/navigationcreator.py
+++ application/navigationcreator.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 class NavigationCreator:
 
@@ -11,7 +12,8 @@ class NavigationCreator:
 
     def _walk(self, path):
         for root, dirs, files in os.walk(path):
-            level = self._get_relative_root(root)
+            level = self._parse_level(root)
+            files = self._parse_filenames(files)
             contents = {
                 "root": root,
                 "level": level,
@@ -26,9 +28,35 @@ class NavigationCreator:
                     contents["dirs"].append(dir_files)
             return contents
 
+    def _parse_level(self, root):
+        relative = self._get_relative_root(root)
+        without_prefix = self._strip_numeric_prefix(relative)
+        level = self._create_title_from_slug(without_prefix)
+        return level
+
+    def _strip_numeric_prefix(self, text):
+        stripped = re.sub(r'^[0-9_-]*(.*)', r'\1', text)
+        return stripped
+
     def _get_relative_root(self, root):
         last_slash = root.rfind('/')
         if not last_slash:
             return root
         cut_from = last_slash + 1
         return root[cut_from:]
+
+    def _create_title_from_slug(self, text):
+        replaced_text = re.sub(r'[-_]+', ' ', text)
+        stripped_text = replaced_text.strip()
+        return stripped_text.title()
+
+    def _parse_filenames(self, files):
+        name = self._parse_filename
+        files = [{'name':name(x), 'path':x } for x in files]
+        return files
+
+    def _parse_filename(self, filename):
+        extension = filename.find('.')
+        without_extension = filename[:extension]
+        without_prefix = self._strip_numeric_prefix(without_extension)
+        return self._create_title_from_slug(without_prefix)
