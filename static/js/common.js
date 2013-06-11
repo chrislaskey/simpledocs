e7aaa94a20f937366cddd1d27937051b5d983384
Chris Laskey <contact@chrislaskey.com>
2013-06-11 15:44:22 -0400

    Added JS, made main nav work

---
+++ static/js/common.js
@@ -0,0 +1,17 @@
+app = {}
+
+app.addNavigationLinks = function(){
+	
+	$('nav.primary')
+		.find('select')
+		.each(function(){
+			$(this).on('change', function(e){
+				var value = $(this).val();
+				if( value ){ window.location = value; }
+			});
+		});
+}
+
+$(document).ready(function(){
+	app.addNavigationLinks();
+});
