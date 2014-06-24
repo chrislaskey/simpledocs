f69f6692149b6c5988650ecde3f9e4aad4b398de
Chris Laskey <contact@chrislaskey.com>
2014-06-24 09:45:25 -0400

    Moved last route code, error handlers, into separate view file

---
+++ app/views/errors.py
@@ -0,0 +1,20 @@
+from flask import render_template, g
+from .. import nav
+from .. import app
+
+from .. helpers.pageprocessing import common_page_processing
+
+
+@app.errorhandler(404)
+def not_found(error):
+    common_page_processing()
+    g.templatevars['nav_items'] = nav.items(app)
+    return render_template('errors/404.html', **g.templatevars), 404
+
+
+@app.errorhandler(500)
+def server_error(error):
+    common_page_processing()
+    g.templatevars['nav_items'] = nav.items(app)
+    return render_template('errors/500.html', **g.templatevars), 500
+
