7e32555c9c36211503cdebc819d6e1f7d651bced
Chris Laskey <contact@chrislaskey.com>
2014-06-24 10:44:37 -0400

    Removed deprecated common processing code

--- app/helpers/pageprocessing.py
+++
@@ -1,6 +0,0 @@
-from flask import g, request
-from .. lib.templateparser import TemplateVariableParser
-
-
-def common_page_processing():
-    g.templatevars = TemplateVariableParser().parse(request)
