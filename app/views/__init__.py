654529a52123ac960b892030f2d182d692d382e4
Chris Laskey <contact@chrislaskey.com>
2014-06-24 09:43:03 -0400

    Removed deprecated routes code

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -8,32 +8,6 @@ from . import document
 from . import search
 
 
-# @app.route('/parse-search', methods = ['post'])
-# def parse_search():
-#     redirect_to = url_for('search_page', terms=search.parse(request))
-#     return redirect(redirect_to)
-
-
-# @app.route('/search/', defaults={'terms': ''})
-# @app.route('/search/<path:terms>')
-# def search_page(terms):
-#     common_page_processing()
-#     search_results = search.results(terms)
-#     http_code = 200 if search_results['success'] else 404
-#     g.templatevars['search_results'] = search_results
-#     g.templatevars['nav_items'] = nav.items(app)
-#     return render_template('site/search.html', **g.templatevars), http_code
-
-
-# @app.route('/', defaults={'path': ''})
-# @app.route('/<path:path>')
-# def page(path):
-#     common_page_processing()
-#     g.templatevars['content'] = document(path)
-#     g.templatevars['nav_items'] = nav.items(app)
-#     return render_template('page.html', **g.templatevars)
-
-
 @app.errorhandler(404)
 def not_found(error):
     common_page_processing()
