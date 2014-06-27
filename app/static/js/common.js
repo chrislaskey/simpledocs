918f6d0260c2887aa0a778c65ddee8f4de8e0a0f
Chris Laskey <contact@chrislaskey.com>
2014-06-27 16:22:21 -0400

    Updated design with fluid/fixed toggle

--- app/static/js/common.js
+++ app/static/js/common.js
@@ -1,7 +1,6 @@
 var app = {}
 
 app.addNavigationLinks = function(){
-	
 	$('nav.primary')
 		.find('select')
 		.each(function(){
@@ -12,6 +11,39 @@ app.addNavigationLinks = function(){
 		});
 }
 
+app.addPageResize = function(){
+	var button = $('#button-resize'),
+		body = $('body'),
+		cookie = $.cookie('resize');
+
+	if( ! cookie ) {
+		body.addClass('fixed');
+		$.cookie('resize', 'fixed', { expires: 30, path: '/' });
+	}else{
+		body.addClass(cookie);
+	}
+
+	button.on('click', function(){
+		cookie = $.cookie('resize');
+
+		if( cookie == 'fluid' ){
+			body
+				.addClass('fixed')
+				.removeClass('fluid');
+			$.cookie('resize', 'fixed', { expires: 30, path: '/' });
+		}else{
+			body
+				.addClass('fluid')
+				.removeClass('fixed');
+			$.cookie('resize', 'fluid', { expires: 30, path: '/' });
+		}
+	});
+}
+
+
+// Document ready
+
 $(document).ready(function(){
 	app.addNavigationLinks();
+	app.addPageResize();
 });
